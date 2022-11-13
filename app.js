b2853b60ce1b24758d6835eee7ef72b5c4eb132e
dansusman <frenchminister@gmail.com>
2022-11-13 10:40:11 -0500

    Finish lab

---
+++ app.js
@@ -0,0 +1,10 @@
+import express from "express";
+import HelloController from "./controllers/hello-controller.js";
+import UserController from "./controllers/users/users-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+const app = express();
+app.use(express.json());
+HelloController(app);
+UserController(app);
+TuitsController(app);
+app.listen(4000);
