de5f122bd4819fb0a74491a4925ad3559d2ec860
dansusman <frenchminister@gmail.com>
2022-11-13 11:55:53 -0500

    Update for React app

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+import cors from "cors";
 import express from "express";
 import HelloController from "./controllers/hello-controller.js";
-import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+import UserController from "./controllers/users/users-controller.js";
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 HelloController(app);
 UserController(app);
--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -5,7 +5,11 @@ const createTuit = (req, res) => {
     const newTuit = req.body;
     newTuit._id = new Date().getTime() + "";
     newTuit.likes = 0;
+    newTuit.dislikes = 0;
     newTuit.liked = false;
+    newTuit.disliked = false;
+    newTuit.retuits = 0;
+    newTuit.replies = 0;
     tuits.push(newTuit);
     res.json(newTuit);
 };
