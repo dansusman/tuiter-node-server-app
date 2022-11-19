04b67d0ba98a6b4e781a64844f19f0b34f08c9ad
dansusman <frenchminister@gmail.com>
2022-11-19 13:47:37 -0500

    Add CONNECTION_STRING

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ import HelloController from "./controllers/hello-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 
-mongoose.connect("mongodb://localhost:27017/tuiter");
+// mongodb+srv://susman:<password>@cluster0.cjawhyp.mongodb.net/?retryWrites=true&w=majority
+
+const CONNECTION_STRING =
+    process.env.DB_CONNECTION_STRING || "mongodb://localhost:27017/tuiter";
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors());
 app.use(express.json());
